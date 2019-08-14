22c92fd7771a7aeb3a1a7c76372aea32959d49e2
Mark Bergstrom <markbergstrom@gmail.com>
2019-08-13 21:06:37 -0500

    added error handling

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ function logResults(jsonResponse, gitHubHandle) {
     console.log(jsonResponse);
     $('.repos').empty();
     $('.repos').removeClass('hidden');
-    if (jsonResponse.message === "Not Found") {
-        console.log("user not found!");
-        $('.repos').append(`<div class="user-not-found">User not found!</div>`);
-    }
-    else if (jsonResponse.length == 0) {
-        console.log("no repos!");
-        $('.repos').append(`<div class="no-repos">This user doesn't have any public repos!</div>`);
+    if (jsonResponse.length == 0) {
+        $('.repos').append(`<div class="no-repos"><h3>This user doesn't have any public repos!</h3></div>`);
     }
     else {
         for (i = 0; i < jsonResponse.length; i++) {
-            $('.repos').append(`<div class="repo-name">${jsonResponse[i].name}</div>
+            $('.repos').append(`<div class="repo-name"><h2>${jsonResponse[i].name}</h2></div>
                 <a class="repo-url" href="${jsonResponse[i].html_url}">${jsonResponse[i].html_url}</a>`);
         }
     }
@@ -21,9 +16,20 @@ function logResults(jsonResponse, gitHubHandle) {
 function getRepos(gitHubHandle) {
     let fetchUrl = "https://api.github.com/users/" + gitHubHandle + "/repos";
     fetch(fetchUrl)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                throw new Error(response.statusText);
+            }
+        })
         .then(jsonResponse => logResults(jsonResponse, gitHubHandle))
-        .catch(error => alert("something's wrong!"));
+        .catch(error => {
+            $('.repos').empty();
+            $('.repos').append(`<div class="error"><h3>Bad Request: ${error.message}!!!</h3></div>`);
+            $('.repos').removeClass('hidden');
+        });
 }
 
 function whichHandle() {
